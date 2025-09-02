9ca04367f046c116a6d1ff2933eaa0f35e45b3d9
Ivan Dave V. Sanipa <sanipa977@gmail.com>
2025-09-02 17:08:55 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("ALL TOO WELL [Taylor Swift]");
+        System.out.println("Well, maybe we got lost in translation, maybe I asked for too much");
+        System.out.println("But maybe this thing was a masterpiece 'til you tore it all up");
+        System.out.println("Runnin' scared, I was there");
+        System.out.println("I remember it all too well");
+        System.out.println("And you call me up again just to break me like a promise");
+        System.out.println("So casually cruel in the name of bein' honest");
+        System.out.println("I'm a crumpled-up piece of paper lyin' here");
+        System.out.println("'Cause I remember it all, all, all");
+    }
+}
