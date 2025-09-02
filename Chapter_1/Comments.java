612a257bf67091fdc833db12062c13a35d8df606
Ivan Dave V. Sanipa <sanipa977@gmail.com>
2025-09-02 17:36:41 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * Basic example of comments in Java.
+ */
+public class Comments {
+    
+    public static void main(String[] args) {
+
+        // Single-line comment
+        System.out.println("Hello!");
+
+        /* 
+         * Multi-line comment
+         * about the next line.
+         */
+        System.out.println("Bye!");
+    }
+}
