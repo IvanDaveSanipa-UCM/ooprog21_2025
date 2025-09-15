6443fcf1a39fc3b0fff7a2bd7187c6785340b56f
Ivan Dave V. Sanipa <sanipa977@gmail.com>
2025-09-15 21:50:44 +0800

    Update CompanyInfo.java

--- Chapter 3/CompanyInfo.java
+++ Chapter 3/CompanyInfo.java
@@ -2,10 +2,10 @@ public class CompanyInfo {
 
     public static void main(String[] args) {
         System.out.println("Ivan Electronics");
-        displayHours();
+        DisplayHours();
     }
 
-    public static void displayHours() {
+    public static void DisplayHours() {
         System.out.println("Monday - Friday   8 am to 6 pm");
         System.out.println("Saturday          8 am to noon");
         System.out.println("Sunday            closed");
