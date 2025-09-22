67e3c967ee24144d96861de54a276303808f8d1d
Ivan Dave V. Sanipa <sanipa977@gmail.com>
2025-09-22 08:41:43 +0800

    Create UsingJOptionPaneDialogs.java

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class UsingJOptionPaneDialogs {
+    public static void main(String[] args) {
+        while (true) {
+            String name = JOptionPane.showInputDialog(null, "Enter your name:");
+
+            if (name == null) {
+                System.exit(0);
+            }
+
+            int confirm = JOptionPane.showConfirmDialog(null,
+                    "Do you want to display your name: " + name + "?",
+                    "Confirm Name",
+                    JOptionPane.YES_NO_OPTION);
+
+            if (confirm == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Hello, " + name);                 
+                break; 
+            }
+        }
+    }
+}
+
